b559796801edc10802a67285d792cd4deeaa6ee4
CameronTheDeveloper <Cameronposey@outlook.com>
2024-03-08 09:56:51 -0800

    Add createHashMap hash() method

--- src/hashmap.js
+++ src/hashmap.js
@@ -1,7 +1,17 @@
 const createHashMap = () => {
 
-    return {};
-};
+    const hash = (key) => {
+        let hashCode = 0;
+        const primeNum = 31;
 
+        for (let i = 0; i < key.length; i++) {
+            hashCode = primeNum * hashCode + key.charCodeAt(i);
+        }
+
+        return hashCode;
+    };
+
+    return { hash };
+};
 
 export { createHashMap };
