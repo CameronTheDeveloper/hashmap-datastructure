186aab37ff65f4a34c7e2b9109ea316cceede284
CameronTheDeveloper <Cameronposey@outlook.com>
2024-03-11 12:19:50 -0700

    Add bucketLinkedList append method

--- src/bucket-linked-list.js
+++ src/bucket-linked-list.js
@@ -4,8 +4,20 @@ const bucketLinkedList = () => {
     return {
         head: null,
         tail: null,
+        size: 0,
 
+        append(key, value) {
+            const newNode = mapNode(key, value);
 
+            if (this.size < 1) {
+                this.head = newNode;
+                this.tail = newNode;
+            } else {
+                this.tail.next = newNode;
+                this.tail = newNode;
+            }
+            this.size++;
+        }
     };
 };
 
